657cc06076eb6d183df7565317b20471c02017d1
Nahid_Hasan <jeweljewel702@gmail.com>
2020-10-02 05:54:47 +0600

    shopping cart with Javascript

--- shoppingCartJS/cartjs.js
+++ shoppingCartJS/cartjs.js
@@ -1,10 +1,10 @@
 let productList=[
-    {ID:1,ProductName:'Ford Mustang ',Price:5308080,PhotoURL:'img/ford.jpg'},
-    {ID:2,ProductName:'Maserati-GranTurismo ',Price:4378080,PhotoURL:'img/maecedes.jpg'},
-    {ID:3,ProductName:'Mercedes-Benz ',Price:3358080,PhotoURL:'img/ford.jpg'},
-    {ID:4,ProductName:'Lamborghini ',Price:3350080,PhotoURL:'img/Maserati-GranTurismo8.png'},
-    {ID:5,ProductName:'Accent ',Price:2458080,PhotoURL:'img/ford.jpg'},
-    {ID:6,ProductName:'Toyota ',Price:1858080,PhotoURL:'img/Maserati-GranTurismo8.png'},
+    {ID:1,ProductName:'Ford Mustang ',Price:5308080,PhotoURL:'img/ford.jpg',Amount:1},
+    {ID:2,ProductName:'Maserati-GranTurismo ',Price:4378080,PhotoURL:'img/maecedes.jpg',Amount:1},
+    {ID:3,ProductName:'Mercedes-Benz ',Price:3358080,PhotoURL:'img/ford.jpg',Amount:1},
+    {ID:4,ProductName:'Lamborgghini ',Price:3350080,PhotoURL:'img/Maserati-GranTurismo8.png',Amount:1},
+    {ID:5,ProductName:'Accent ',Price:2458080,PhotoURL:'img/ford.jpg',Amount:1},
+    {ID:6,ProductName:'Toyota ',Price:1858080,PhotoURL:'img/Maserati-GranTurismo8.png',Amount:1},
 ];
 
 
@@ -16,14 +16,19 @@ let productItemtemplate=`<div class="cart">
     <h4>{{ProductName}}</h4>
     <h4>৳ {{Price}} </h4>
     <button class="addToCart" onclick="chosenItem({{ID}})" type="button">Add to cart</button>
+    
 </div>
 </div>`;
 
 
-let rowtemplate=` <tr>
+let rowtemplate=` <div>
+<tr>
 <td>{{ProductName}}</td>
 <td>{{Price}}</td>
-</tr>`;
+<td>{{Amount}}</td>
+</tr>
+
+</div>`;
 
 function createProductList()
 {
@@ -42,36 +47,122 @@ function createProductList()
 
 
 }
+
+let cartRowTemplate=`  <tr>             
+<td >{{ProductName}}</td>
+<td >{{Price}}</td>
+<td >{{Amount}}</td>
+</tr>`;
+
+let cartheadTemplate=`<tr>
+<th style="width:140px">Product Name</th>
+<th>Price</th>
+<th>Amount</th>
+</tr>`;
+
+function createCartList()
+{
+   
+    let ph= document.getElementById("tbllist");
+    let content="";
+    cartItems.forEach(item=>{
+        let pt=cartRowTemplate;
+        pt=pt.replace("{{ProductName}}",item.ProductName);
+        pt=pt.replace("{{Price}}",item.Price);
+        pt=pt.replace("{{Amount}}",item.Amount);
+        content+=pt;
+    });
+    ph.innerHTML=cartheadTemplate+content;
+
+
+}
+
 function chosenItem(id){
 
    let selectedProduct = productList.find(c=>c.ID==id);
    //let pList = rowtemplate;
 
    let table = document.getElementById('tbllist');
-
+   //let ProductName = document.getElementById('ProductName');
+    //let Price = document.getElementById('Price');
     
     let row = document.createElement('tr');
 
     let cell1 = document.createElement('td');
     cell1.innerText=selectedProduct.ProductName;
+
      let cell2 = document.createElement('td');
      cell2.innerText=selectedProduct.Price;
 
+     let count =0;
+     cartItems.forEach(
+         item=>
+         {
+             if(item.ID==selectedProduct.ID)
+             {
+                count++;
+             }
+         }
+         );
+         
+
+
+     let cell3 = document.createElement('td');
+     cell3.innerText=count +1;
+     //Amount = cell3.innerText;
+ 
+    
+
      row.appendChild(cell1);
      row.appendChild(cell2);
+     row.appendChild(cell3);
 
      table.appendChild(row);
 
-     cartItems.push(selectedProduct);
+     if(count==0) //new item
+     {
+         cartItems.push(selectedProduct);
+     }
+     else
+     {
+         cartItems.forEach(item=>{
+        if(item.ID==selectedProduct.ID)
+        {
+            item.Amount=item.Amount+1;
+            
+        }
+       });
+    }
+   
+
+    createCartList();
+
     
      let sum=0;
-     cartItems.forEach(item=>{
-     sum+=item.Price;
-     });
 
+     if(cartItems==1)
+     {
+
+         cartItems.forEach(item=>{
+         sum+=item.Price;
+        });
+
+     }
+     else {
+
+        cartItems.forEach(item=>{
+            sum+=(item.Price*item.Amount);
+           });
+
+
+     }
+     
      document.getElementById("totalPrice").innerText=sum;
+     
 
 }
+
+
 
 
 function OnBodyLoad()
